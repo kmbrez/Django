67dd67830246f6d16e25b537a45bf9abba867fef
Kevin Brez <kbrez@penny-newman.com>
2016-04-12 16:28:23 -0700

    Added players model

--- BracketProject/BracketProject/BracketProject/settings.py
+++ BracketProject/BracketProject/BracketProject/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'picks',
     'app',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
---
+++ BracketProject/BracketProject/picks/models.py
@@ -0,0 +1,9 @@
+﻿from django.db import models
+
+class Player(models.Model):
+    name = models.CharField(max_length=255)
+    added = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.name
