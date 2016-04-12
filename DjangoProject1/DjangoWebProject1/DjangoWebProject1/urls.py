eeba4df5b92aed660e161168a5cde5130716536a
Kevin Brez <kbrez@penny-newman.com>
2016-04-12 14:35:36 -0700

    Got admin working

--- DjangoProject1/DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoProject1/DjangoWebProject1/DjangoWebProject1/urls.py
@@ -14,6 +14,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^courses/', include('courses.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'app.views.home', name='home'),
     url(r'^contact$', 'app.views.contact', name='contact'),
     url(r'^about', 'app.views.about', name='about'),
@@ -38,7 +39,4 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
