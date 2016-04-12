305c9e1119ac7450087c9e6186bf877bb362f28f
Kevin Brez <kbrez@penny-newman.com>
2016-04-12 14:14:09 -0700

    Created courses view

--- DjangoProject1/DjangoWebProject1/DjangoWebProject1/urls.py
+++ DjangoProject1/DjangoWebProject1/DjangoWebProject1/urls.py
@@ -13,6 +13,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^courses/', include('courses.urls')),
     url(r'^$', 'app.views.home', name='home'),
     url(r'^contact$', 'app.views.contact', name='contact'),
     url(r'^about', 'app.views.about', name='about'),
