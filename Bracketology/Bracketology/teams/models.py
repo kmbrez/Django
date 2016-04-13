ae1a52986ef6bf10b1adfe4e030f4d33d49c289d
Kevin Brez <kbrez@penny-newman.com>
2016-04-13 09:02:19 -0700

    Added all NCAA teams + some users

--- Bracketology/Bracketology/teams/models.py
+++ Bracketology/Bracketology/teams/models.py
@@ -4,6 +4,7 @@
 class Team(models.Model):
     name = models.CharField(max_length=200)
     seed = models.IntegerField(default=0)
+    region = models.CharField(max_length=200)
 
 
     def __str__(self):
