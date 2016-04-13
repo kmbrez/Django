8579a5f67c3a2a81d93c4df7775b674191df2a44
Kevin Brez <kbrez@penny-newman.com>
2016-04-13 08:40:54 -0700

    Added teams to admin

---
+++ Bracketology/Bracketology/teams/models.py
@@ -0,0 +1,10 @@
+﻿from django.db import models
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+    seed = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.name
